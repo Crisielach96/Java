a003079da5ab889f24aa0179d5c1eea8f20bc85d
Cristian Sielach <crisielach@yahoo.com.ar>
2019-03-25 11:56:12 -0300

    Add files via upload

---
+++ Clase 1 Sielach 25-3-2019/src/com/utn/proyecto/funcionalidades/MainClass.java
@@ -0,0 +1,33 @@
+package com.utn.proyecto.funcionalidades;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		
+		//Comentario de linea
+		
+		/**
+		 * java DOC
+		 * */
+		
+		/*
+		 * 
+		 * 
+		 * 
+		 * comentario de bloque
+		 * 
+		 * 
+		 */
+		
+		int dni=40080375;
+		
+		System.out.println("DNI: "+dni);//imprime  variable
+		
+		System.err.println("Error"); //Imprime errores
+
+	}
+
+}
