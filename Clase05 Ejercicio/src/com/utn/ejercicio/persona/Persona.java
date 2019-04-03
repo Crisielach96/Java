7d7c570f6ccb75049d871dcd6c5131dcd06c40eb
Cristian Sielach <crisielach@yahoo.com.ar>
2019-04-03 12:07:29 -0300

    Add files via upload

--- Clase05 Ejercicio/src/com/utn/ejercicio/persona/Persona.java
+++ Clase05 Ejercicio/src/com/utn/ejercicio/persona/Persona.java
@@ -33,6 +33,46 @@ public class Persona {
 		this.dni = this.generarDNI();
 	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
 	int calularIMC(float peso, float altura) {
 		double imc = 0;
 		int retorno = 0;
